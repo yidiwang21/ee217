a8f85e8d1b97f8471124132e37b9c785c5572ff4
yidiwang21 <ydwang21@gmail.com>
2019-03-05 21:02:49 -0800

    add timeline

--- final_project/draw_blocks.py
+++ final_project/draw_blocks.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from pylab import *
 
 sm_num = 15
 kernel_num  = 33
@@ -18,39 +19,70 @@ for i in range (sm_num):
     sm_line_list.append([])
 
 with open(filename) as f:
-    for sm_line_num, line in enumerate(f, 1):
+    for sm_line_num, line in enumerate(f):
         if 'SM id: 0' in line:
             sm_line_list[0].append(sm_line_num)
-        # TODO:
+        # if 'SM id: 1' in line:
+        #     sm_line_list[1].append(sm_line_num)
+        # TODO: will mess up with 1x
+        if 'SM id: 2' in line:
+            sm_line_list[2].append(sm_line_num)
+        if 'SM id: 3' in line:
+            sm_line_list[3].append(sm_line_num)
+        if 'SM id: 4' in line:
+            sm_line_list[4].append(sm_line_num)
+        if 'SM id: 5' in line:
+            sm_line_list[5].append(sm_line_num)
+        if 'SM id: 6' in line:
+            sm_line_list[6].append(sm_line_num)
+        if 'SM id: 7' in line:
+            sm_line_list[7].append(sm_line_num)
+        if 'SM id: 8' in line:
+            sm_line_list[8].append(sm_line_num)
+        if 'SM id: 9' in line:
+            sm_line_list[9].append(sm_line_num)
+        if 'SM id: 10' in line:
+            sm_line_list[10].append(sm_line_num)
+        if 'SM id: 11' in line:
+            sm_line_list[11].append(sm_line_num)
+        if 'SM id: 12' in line:
+            sm_line_list[12].append(sm_line_num)
+        if 'SM id: 13' in line:
+            sm_line_list[13].append(sm_line_num)
+        if 'SM id: 14' in line:
+            sm_line_list[14].append(sm_line_num)
 
 for row in range(sm_num):
     cols = len(sm_line_list[row])
-    print("Row", row, "has", cols, "columns: ", end="")
+    print("\n Row", row, "has", cols, "columns: ", end="")
     for col in range(cols):
         print(sm_line_list[row][col], " ", end="")
 
-idx = 0
-with open(filename) as f:
-    for line_num, line in enumerate(f, 1):
-        if line_num == sm_line_list[0][idx] + 1:   # start_time line @smid = 0
-            a, b = line.split(":")
-            b = int(b)
-            start_time_list[0].append(b)
-            idx += 1
-        if line_num == sm_line_list[0][idx] + 2:   # stop_time line @smid = 0
-            a, b = line.split(":")
-            b = int(b)
-            stop_time_list[0].append(b)
-            idx += 1
+for i in range(sm_num - 1):
+    idx = 0
+    with open(filename) as f:
+        for ln, tl in enumerate(f):
+            if idx >= len(sm_line_list[i]):
+                break;
+            if ln == sm_line_list[i][idx] + 1:   # start_time line @smid = 0
+                a, b = tl.split(":")
+                b = int(b)
+                start_time_list[i].append(b)
+            if ln == sm_line_list[i][idx] + 2:   # stop_time line @smid = 0
+                a, b = tl.split(":")
+                b = int(b)
+                stop_time_list[i].append(b)
+                idx += 1
 
 # now we get the start/stop time of each block @smid = 0
 # plot
 height = 1
-fig, ax = plt.subplots(nrows=15, ncols=1)
-for i in range (0, 14):
-    for j in range (0, len(start_time_list[i])):
-        ax.plot(start_time_list[i][j], stop_time_list[i][j])
+# fig, ax = plt.subplots(nrows=15, ncols=1)
+plt.figure()
+for i in range (sm_num - 1):
+    plt.subplot(sm_num - 1, 1, i+1)
+    for j in range (len(start_time_list[i])):
+        plt.hlines(y=height, xmin=int(start_time_list[i][j]), xmax=int(stop_time_list[i][j]), lw=2)
+        height += 1;
 
-
-
-
+plt.show()
